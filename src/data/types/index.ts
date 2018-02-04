973d6735c3f38d814db60ff6b832ff4aa42b9958
Luca Ronca <luca.ronca07@gmail.com>
2018-02-04 20:23:20 +0100

    Refactor typeDefs without merge-graphql-schemas

--- src/data/types/index.ts
+++ src/data/types/index.ts
@@ -1,28 +1,25 @@
 import { promisify } from 'bluebird'
 import { readFile as nodeReadFile, readdir as nodeReaddir } from 'fs'
 import { resolve, extname } from 'path'
-
-import { mergeTypes } from 'merge-graphql-schemas'
+import { pipeP } from 'ramda'
 
 const readFile = promisify(nodeReadFile)
 const readdir = promisify(nodeReaddir)
 
-export async function getTypesFilePaths(): Promise<string[]> {
-  const fileNames: string[] = await readdir(__dirname)
+export async function getTypesFilePaths(dir: string): Promise<string[]> {
+  const fileNames: string[] = await readdir(dir)
 
   return fileNames
     .filter(fileName => extname(fileName) === '.gql')
-    .map(fileName => resolve(__dirname, fileName))
+    .map(fileName => resolve(dir, fileName))
 }
 
-export async function getTypesData(): Promise<string[]> {
-  const paths: string[] = await getTypesFilePaths()
-
+export async function getTypesData(paths: string[]): Promise<string[]> {
   return Promise.all(paths.map(filePath => readFile(filePath, 'utf8')))
 }
 
-export default async (): Promise<string> => {
-  const typesArray: string[] = await getTypesData()
-
-  return mergeTypes(typesArray)
+export default async (): Promise<string[]> => {
+  return pipeP<string, string[], string[]>(getTypesFilePaths, getTypesData)(
+    __dirname,
+  )
 }
